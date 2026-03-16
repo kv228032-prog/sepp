3c06bcbba5d90a137ec27934c80afa3ce546faea
kv228032-prog <kv228032@gmail.com>
2026-03-16 11:07:02 +0530

    Add files via upload

---
+++ class even.java
@@ -0,0 +1,19 @@
+import java.util Scanner;
+public class even
+{
+class even odd
+{
+public stativ void main (String a[])
+{
+Scanner Sc=new Scanner(System.in)
+System.out.println("enter the number")
+int num=Sc.nextint();
+if(num%2==0)
+System.out.println(num+"is"+"even");
+}
+else
+{
+System.out.println(num+"is"+"odd");
+}
+}
+}
